b5eea02673841f83a724adc1c4fe9d21052ba086
Harish <harishhammett77@gmail.com>
2024-08-18 16:46:37 +0545

    SecondCommit

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,20 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import secrets
+
+
+secret_key = secrets.token_hex(16)
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'sdfasdfasdf asdfasdf'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = secret_key
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
